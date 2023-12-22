310af5d79f7dfe2aa0b35848ffec00f4c2fa0a8e
Esteban Sanchez <exloz@hotmail.com>
2023-12-22 17:54:42 -0500

    Added annotation to repositories

--- src/main/java/com/granbuda/backbingo/repository/GameSetRepository.java
+++ src/main/java/com/granbuda/backbingo/repository/GameSetRepository.java
@@ -2,6 +2,8 @@ package com.granbuda.backbingo.repository;
 
 import com.granbuda.backbingo.model.GameSet;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameSetRepository extends MongoRepository<GameSet, String> {
 }
