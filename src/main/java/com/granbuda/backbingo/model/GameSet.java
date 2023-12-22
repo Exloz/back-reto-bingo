c6cbd8e159aa6c769fffac8c55d2f2364b05f38d
Esteban Sanchez <exloz@hotmail.com>
2023-12-22 14:16:32 -0500

    Created GameSet model

--- src/main/java/com/granbuda/backbingo/model/GameSet.java
+++ src/main/java/com/granbuda/backbingo/model/GameSet.java
@@ -1,4 +1,39 @@
 package com.granbuda.backbingo.model;
 
+import java.util.List;
+
 public class GameSet {
+    private String id;
+    private List<Integer> ballots;
+    private List<String> userIds;
+
+    public GameSet(String id, List<Integer> ballots, List<String> userIds) {
+        this.id = id;
+        this.ballots = ballots;
+        this.userIds = userIds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Integer> getBallots() {
+        return ballots;
+    }
+
+    public void setBallots(List<Integer> ballots) {
+        this.ballots = ballots;
+    }
+
+    public List<String> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<String> userIds) {
+        this.userIds = userIds;
+    }
 }
