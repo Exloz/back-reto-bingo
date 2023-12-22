fddab9b9fbb497fb06379c1017a919b9642debe0
Esteban Sanchez <exloz@hotmail.com>
2023-12-22 13:46:06 -0500

    first commit

---
+++ src/main/java/com/granbuda/backbingo/BackBingoApplication.java
@@ -0,0 +1,13 @@
+package com.granbuda.backbingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackBingoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/granbuda/backbingo/BackBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.granbuda.backbingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
