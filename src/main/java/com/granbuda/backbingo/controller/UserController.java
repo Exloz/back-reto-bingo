dd29c275d3745ecb373c91a9f9c8c975f833c3ee
Esteban Sanchez <exloz@hotmail.com>
2023-12-22 18:13:47 -0500

    Minor adjustments

--- src/main/java/com/granbuda/backbingo/controller/UserController.java
+++ src/main/java/com/granbuda/backbingo/controller/UserController.java
@@ -5,12 +5,9 @@ import com.granbuda.backbingo.model.dto.UserResponse;
 import com.granbuda.backbingo.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
@@ -23,9 +20,9 @@ public class UserController {
     @PostMapping("/createWithBingoTable")
     public ResponseEntity<UserResponse> createUserWithBingoTable(@RequestBody UserRequest userRequest){
         try{
-            return new ResponseEntity<UserResponse>(userService.createUserWithBingoTable(userRequest), HttpStatus.CREATED);
+            return new ResponseEntity<>(userService.createUserWithBingoTable(userRequest), HttpStatus.CREATED);
         } catch (Exception e){
-            return new ResponseEntity<UserResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
--- src/main/java/com/granbuda/backbingo/repository/UserBingoTableRepository.java
+++ src/main/java/com/granbuda/backbingo/repository/UserBingoTableRepository.java
@@ -2,8 +2,6 @@ package com.granbuda.backbingo.repository;
 
 import com.granbuda.backbingo.model.UserBingoTable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserBingoTableRepository extends MongoRepository<UserBingoTable, String> {
 }
