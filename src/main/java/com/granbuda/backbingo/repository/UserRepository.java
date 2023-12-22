7d69fa67c132e8c676b5838ad735015ebe7dd34d
Esteban Sanchez <exloz@hotmail.com>
2023-12-22 16:09:12 -0500

    Added additional method to Optional User

--- src/main/java/com/granbuda/backbingo/repository/UserRepository.java
+++ src/main/java/com/granbuda/backbingo/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.granbuda.backbingo.repository;
 import com.granbuda.backbingo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findById(String id);
 }
